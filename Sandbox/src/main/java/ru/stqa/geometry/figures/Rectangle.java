9effe583b65ac654d471adb39e8718a584df80f0
kirillsemigin <kirillsemigin@mail.ru>
2025-01-19 21:21:38 +0300

    Структуризация кода

--- Sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ Sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,7 +2,8 @@ package ru.stqa.geometry.figures;
 
 public class Rectangle {
     public static void printRectangleArea(double a, double b) { // выводим информацию о площади квадарата
-        System.out.println("Площадь прямоугольника со стронами " + a + " и " + b + " = " + rectangleArea(a, b));
+       var text = String.format("Площадь прямоугольника со стронами %f и %f = %f", a, b, rectangleArea(a, b));
+        System.out.println(text);
     }
 
     private static double rectangleArea(double a, double b) { // производим расчет площади квадрата
