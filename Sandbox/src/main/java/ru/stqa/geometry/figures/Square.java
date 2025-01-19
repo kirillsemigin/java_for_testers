2016002369ec2d6514f22d2b3670f4b23f8b1588
kirillsemigin <kirillsemigin@mail.ru>
2025-01-19 21:42:18 +0300

    Созданы первые пробные тесты. Повторил за тренером.

--- Sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ Sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,11 +2,15 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea(double side) { // выводим информацию о площади треугольника
-        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, Area(side));
         System.out.println(text);
     }
 
-    private static double squareArea(double a) { // производим расчет площади треугольника
+    public static double Area(double a) { // производим расчет площади треугольника
         return a * a;
+    }
+
+    public static double perimeter(double a) {
+        return 4 * a;
     }
 }
