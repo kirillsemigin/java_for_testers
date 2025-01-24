29f0b62e006b14b07de3ac84f37ac6b07707df1f
kirillsemigin <kirillsemigin@mail.ru>
2025-01-24 21:47:50 +0300

    Добавлены тесты Rectangle

--- Sandbox/src/main/java/Hello.java
+++ Sandbox/src/main/java/Hello.java
@@ -1,23 +1,17 @@
-import java.io.File;
-
 public class Hello {
     public static void main(String[] args) {
 
-        try {
-            var z = calculate();
+        var x = 1;
+        var y = 1;
+        if (y == 0) {
+            System.out.println("Division by zero is not allowed");
+        } else {
+            var z = devide(x, y);
             System.out.println("Hello, world!");
-            System.out.println(z);
-    }   catch (ArithmeticException exception) {
-            exception.printStackTrace();
-        }
         }
 
-    private static int calculate() {
-        var x = 1;
-        var y = 0;
-        var z = devide(x, y);
-        return z;
     }
+
 
     private static int devide(int x, int y) {
         var z = x / y;
