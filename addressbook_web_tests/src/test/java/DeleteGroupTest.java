64e2fadc33fddbdbaaacb6f9bb3ccecea2f32b3b
kirillsemigin <kirillsemigin@mail.ru>
2025-01-30 20:14:04 +0300

    Оптимизация импортов. Удаление неиспользуемых переменных.

--- addressbook_web_tests/src/test/java/DeleteGroupTest.java
+++ addressbook_web_tests/src/test/java/DeleteGroupTest.java
@@ -3,40 +3,36 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class DeleteGroupTest {
-  private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  @BeforeEach
-  public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-  }
-  @AfterEach
-  public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void deleteGroupTest() {
-    driver.get("http://localhost/addressbook/group.php");
-    driver.manage().window().setSize(new Dimension(1936, 1056));
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-    driver.findElement(By.name("selected[]")).click();
-    driver.findElement(By.name("delete")).click();
-    driver.findElement(By.linkText("group page")).click();
-    driver.findElement(By.linkText("Logout")).click();
-  }
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void deleteGroupTest() {
+        driver.get("http://localhost/addressbook/group.php");
+        driver.manage().window().setSize(new Dimension(1936, 1056));
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
+        driver.findElement(By.linkText("Logout")).click();
+    }
 }
