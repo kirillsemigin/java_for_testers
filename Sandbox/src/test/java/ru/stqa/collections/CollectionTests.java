8e8ea3f6d8b294cc2d51857042f0b81e2853767f
kirillsemigin <kirillsemigin@mail.ru>
2025-02-14 19:21:10 +0300

    изучение массивов

---
+++ Sandbox/src/test/java/ru/stqa/collections/CollectionTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.collections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CollectionTests {
+    @Test
+    void arrayTests() {
+        var array = new String[]{"a", "b", "c"};
+        Assertions.assertEquals("a", array[0]);
+
+        array[0] = "d";
+        Assertions.assertEquals("d", array[0]);
+    }
+}
