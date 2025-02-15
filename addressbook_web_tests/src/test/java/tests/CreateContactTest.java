665a0772a5d76271665e20cccd673e2140aa2ad8
kirillsemigin <kirillsemigin@mail.ru>
2025-02-15 18:08:22 +0300

    доработка кода
    Добавлен счетчик количества контактов в тесты
    CreateContactTest

--- addressbook_web_tests/src/test/java/tests/CreateContactTest.java
+++ addressbook_web_tests/src/test/java/tests/CreateContactTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import model.ContactData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
@@ -9,9 +10,13 @@ public class CreateContactTest extends TestBase {
 
     @Test
     public void createContact () {
+        int ContactCount = app.contacts().contactCount();
         app.contacts().createContact(new ContactData("firstname", "middlename", "lastname", "nickname", "title", "company",
                 "address", "home", "mobile", "work", "fax", "email", "email2", "email3", "homepage"));
+        int newContactCount = app.contacts().contactCount();
+        Assertions.assertEquals(ContactCount + 1, newContactCount);
     }
+
 
     @Test
     public void createContactWithEmptyFields () {
