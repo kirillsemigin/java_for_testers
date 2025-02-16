58015ce29ccbe42555b520e1d4c07260fc9d4d09
kirillsemigin <kirillsemigin@mail.ru>
2025-02-16 10:36:37 +0300

    рефактор кода контактов.
    убрал доп.поля.
    для создания контактов оставил
    firstname
    middlename
    lastname
    nickname

--- addressbook_web_tests/src/test/java/model/ContactData.java
+++ addressbook_web_tests/src/test/java/model/ContactData.java
@@ -1,47 +1,33 @@
 package model;
 
-public record ContactData (String firstname, String middlename, String lastname, String nickname,
-                           String title, String company, String address, String home, String mobile, String work,
-                           String fax, String email, String email2, String email3, String homepage) {
+public record ContactData (String firstname, String middlename, String lastname, String nickname) {
 
     public ContactData () {
-        this("", "", "", "", "", "", "", "", "", "", "", "", "", "", "");
+        this("", "", "", "");
 
     }
 
     public ContactData withNameOnly(String firstname) {
-        return new ContactData(firstname, this.middlename, this.lastname, this.nickname, this.title,
-                this.company, this.address, this.home, this.mobile, this.work, this.fax, this.email,
-                this.email2, this.email3, this.homepage);
+        return new ContactData(firstname, this.middlename, this.lastname, this.nickname);
     }
 
     public ContactData withMiddleNameOnly(String middlename) {
-        return new ContactData(this.firstname, middlename, this.lastname, this.nickname, this.title,
-                this.company, this.address, this.home, this.mobile, this.work, this.fax, this.email,
-                this.email2, this.email3, this.homepage);
+        return new ContactData(this.firstname, middlename, this.lastname, this.nickname);
     }
     public ContactData withLastNameOnly(String lastname) {
-        return new ContactData(this.firstname, this.middlename, lastname, this.nickname, this.title,
-                this.company, this.address, this.home, this.mobile, this.work, this.fax, this.email,
-                this.email2, this.email3, this.homepage);
+        return new ContactData(this.firstname, this.middlename, lastname, this.nickname);
     }
 
     public ContactData withNickNameOnly(String nickname) {
-        return new ContactData(this.firstname, this.middlename, this.lastname, nickname, this.title,
-                this.company, this.address, this.home, this.mobile, this.work, this.fax, this.email,
-                this.email2, this.email3, this.homepage);
+        return new ContactData(this.firstname, this.middlename, this.lastname, nickname);
     }
 
     public ContactData withTitleOnly(String nickname) {
-        return new ContactData(this.firstname, this.middlename, this.lastname, this.nickname, title,
-                this.company, this.address, this.home, this.mobile, this.work, this.fax, this.email,
-                this.email2, this.email3, this.homepage);
+        return new ContactData(this.firstname, this.middlename, this.lastname, this.nickname);
     }
 
     public ContactData withCompanyOnly(String nickname) {
-        return new ContactData(this.firstname, this.middlename, this.lastname, this.nickname, this.title,
-                company, this.address, this.home, this.mobile, this.work, this.fax, this.email,
-                this.email2, this.email3, this.homepage);
+        return new ContactData(this.firstname, this.middlename, this.lastname, this.nickname);
     }
 
 }
--- addressbook_web_tests/src/test/java/tests/CreateContactTest.java
+++ addressbook_web_tests/src/test/java/tests/CreateContactTest.java
@@ -17,65 +17,26 @@ public class CreateContactTest extends TestBase {
             for (var middlename : List.of("", "test middlename")) {
                 for (var lastname : List.of("", "test last name")) {
                     for (var nickname : List.of("", "test nickname")) {
-                        for (var title : List.of("", "test title")) {
-                            for (var company : List.of("", "test company")) {
-                                for (var address : List.of("", "test address")) {
-                                    for (var home : List.of("", "test home")) {
-                                        for (var mobile : List.of("", "test mobile")) {
-                                            for (var work : List.of("", "test work")) {
-                                                for (var fax : List.of("", "test fax")) {
-                                                    for (var email : List.of("", "test email")) {
-                                                        for (var email2 : List.of("", "test email2")) {
-                                                            for (var email3 : List.of("", "test email3")) {
-                                                                for (var homepage : List.of("", "test homepage")) {
-                                                                    result.add(new ContactData(
-                                                                            firstname,
-                                                                            middlename,
-                                                                            lastname,
-                                                                            nickname,
-                                                                            title,
-                                                                            company,
-                                                                            address,
-                                                                            home,
-                                                                            mobile,
-                                                                            work,
-                                                                            fax,
-                                                                            email,
-                                                                            email2,
-                                                                            email3,
-                                                                            homepage
-                                                                            ));
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                        result.add(new ContactData(firstname, middlename, lastname, nickname));
                     }
                 }
             }
         }
+
         for (int i = 0; i < 2; i++) {
-            result.add(new ContactData(
-                    randomString(i * 2), randomString(i * 2), randomString(i * 2),
-                    randomString(i * 2), randomString(i * 2), randomString(i * 2),
-                    randomString(i * 2), randomString(i * 2), randomString(i * 2),
-                    randomString(i * 2), randomString(i * 2), randomString(i * 2),
-                    randomString(i * 2), randomString(i * 2), randomString(i * 2)));
+            result.add(new ContactData(randomString(i * 2), randomString(i * 2), randomString(i * 2), randomString(i * 2)));
         }
         return result;
     }
+
+
     @ParameterizedTest
     @MethodSource("contactProvider")
-    public void createContact (ContactData contact){
+    public void createContact(ContactData contact) {
         int ContactCount = app.contacts().contactCount();
         app.contacts().createContact(contact);
         int newContactCount = app.contacts().contactCount();
         Assertions.assertEquals(ContactCount + 1, newContactCount);
     }
 }
+
