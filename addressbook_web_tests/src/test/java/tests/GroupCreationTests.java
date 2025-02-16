85f21bdd6e1d84f5f3bd55e2b4d43cff4499102f
kirillsemigin <kirillsemigin@mail.ru>
2025-02-16 09:55:29 +0300

    доработка кода
    лекция 4.7
    сортировка списков
    Доработка тестов
    GroupCreationTest
    GroupModificationTest

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -34,10 +35,19 @@ public class GroupCreationTests extends TestBase {
     @ParameterizedTest
     @MethodSource("groupProvider")
     public void CanCreateMultipleGroups(GroupData group) {
-        int groupCount = app.groups().getCount(); // подсчет количества групп до операции добавления
+        var oldGroups = app.groups().getList();
         app.groups().createGroup(group);
-        int newGroupCount = app.groups().getCount(); // подсчет количества групп после операции добавления
-        Assertions.assertEquals(groupCount + 1, newGroupCount);
+        var newGroups = app.groups().getList();
+        Comparator<GroupData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newGroups.sort(compareById);
+
+        var expectedList = new ArrayList<>(oldGroups);
+        expectedList.add(group.withId(newGroups.get(newGroups.size() - 1).id()).withHeader("").withFooter(""));
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newGroups, expectedList);
+
 
     }
 
@@ -53,9 +63,10 @@ public class GroupCreationTests extends TestBase {
     @ParameterizedTest
     @MethodSource("negativeGroupProvider")
     public void CanNotCreateGroup(GroupData group) {
-        int groupCount = app.groups().getCount(); // подсчет количества групп до операции добавления
+        var oldGroups = app.groups().getList();
         app.groups().createGroup(group);
-        int newGroupCount = app.groups().getCount(); // подсчет количества групп после операции добавления
-        Assertions.assertEquals(groupCount, newGroupCount);
+        var newGroups = app.groups().getList();
+        Assertions.assertEquals(newGroups, oldGroups);
+
     }
 }
