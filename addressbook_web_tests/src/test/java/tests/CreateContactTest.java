65073aaad2e5a19891029410e9d3b6e61e4de0f3
kirillsemigin <kirillsemigin@mail.ru>
2025-02-21 21:01:17 +0300

    доработка кода
    Доработка тестов по добавлению контактов путем сравнения списков.

--- addressbook_web_tests/src/test/java/tests/CreateContactTest.java
+++ addressbook_web_tests/src/test/java/tests/CreateContactTest.java
@@ -1,10 +1,12 @@
 package tests;
 
 import model.ContactData;
+import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CreateContactTest extends TestBase {
@@ -34,9 +36,17 @@ public class CreateContactTest extends TestBase {
     @ParameterizedTest
     @MethodSource("contactProvider")
     public void createContact(ContactData contact) {
-        int ContactCount = app.contacts().contactCount();
+        var oldContacts = app.contacts().getList();
         app.contacts().createContact(contact);
-        int newContactCount = app.contacts().contactCount();
-        Assertions.assertEquals(ContactCount + 1, newContactCount);
+        var newContacts = app.contacts().getList();
+        Comparator<ContactData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newContacts.sort(compareById);
+        var expectedList = new ArrayList<>(oldContacts);
+        expectedList.add(contact.withId(newContacts.get(newContacts.size() -1 ).id()).withMiddleName("").withNickName(""));
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newContacts, expectedList);
+
     }
 }
