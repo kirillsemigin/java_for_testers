cc3723138df13d7d379c3bb0051037ff80ddcc62
kirillsemigin <kirillsemigin@mail.ru>
2025-02-25 20:59:38 +0300

    Revert "Доработка кода"
    This reverts commit b377330d1327c756c67e672b8b193b4527e759d0.

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -8,7 +8,6 @@ import common.CommonFunctions;
 import model.GroupData;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -70,11 +69,7 @@ public class Generator {
         if ("json".equals(format)) {
             ObjectMapper mapper = new ObjectMapper();
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
-            var json = mapper.writeValueAsString(data);
-
-            try (var writer = new FileWriter(output)) {
-                writer.write(json);
-            }
+            mapper.writeValue(new File(output), data);
         } else {
             throw new IllegalArgumentException("Неизвестный формат данных" + format);
         }
