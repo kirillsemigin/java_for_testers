1ab02922a447b4699a8a5bb6f4e80aff441111ff
kirillsemigin <kirillsemigin@mail.ru>
2025-03-25 22:37:54 +0300

    Добавлен файл ci.properties Добавлена возможность запуска тестов через Selenium Server

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -4,9 +4,15 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -20,15 +26,28 @@ public class ApplicationManager {
 
     private Properties properties;
 
-   public void init(String Browser, Properties properties) {
+   public void init(String Browser, Properties properties) throws MalformedURLException {
        this.properties = properties;
         if (driver == null) {
+            var seleniumServer = properties.getProperty("seleniumServer");
             if ("Chrome".equals(Browser)) {
-                driver = new ChromeDriver();
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new ChromeOptions());
+                } else {
+                    driver = new ChromeDriver();
+                }
             } else if ("Edge".equals(Browser)) {
-                driver = new EdgeDriver();
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new EdgeOptions());
+                } else {
+                    driver = new EdgeDriver();
+                }
             } else if ("Firefox".equals(Browser)) {
-                driver = new FirefoxDriver();
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new FirefoxOptions());
+                } else {
+                    driver = new FirefoxDriver();
+                }
             } else
                 throw new IllegalArgumentException(String.format("Unknown browser %s", Browser));
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
